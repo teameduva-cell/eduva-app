0523fdf54d1aa8d5dc41a05b20e28657b35ac724
teameduva-cell <team.eduva@gmail.com>
2026-09-08 11:04:20 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -41,4 +41,4 @@ export default async function handler(req, res) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Server error: ' + error.message });
   }
-}
+};
